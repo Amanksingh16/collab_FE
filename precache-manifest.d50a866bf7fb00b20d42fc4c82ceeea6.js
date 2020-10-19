a5ff55fb69df3fb8c5839ee5b94e6f22dbe8f2ee
Aman Singh <amansingh16597@gmail.com>
2020-10-19 13:47:15 -0700

    Updates

---
+++ precache-manifest.d50a866bf7fb00b20d42fc4c82ceeea6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6179b87bd8c821f21372fd860cbbe72",
+    "url": "/Amanksingh16/collab_FE/index.html"
+  },
+  {
+    "revision": "7a065d26a2e1c1a5539b",
+    "url": "/Amanksingh16/collab_FE/static/css/main.bb188fb2.chunk.css"
+  },
+  {
+    "revision": "25476cd039c93be72204",
+    "url": "/Amanksingh16/collab_FE/static/js/2.6a7970c9.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/Amanksingh16/collab_FE/static/js/2.6a7970c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a065d26a2e1c1a5539b",
+    "url": "/Amanksingh16/collab_FE/static/js/main.cce65034.chunk.js"
+  },
+  {
+    "revision": "a42ada4fc4bc63b5112b",
+    "url": "/Amanksingh16/collab_FE/static/js/runtime-main.4f9aa1a1.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Amanksingh16/collab_FE/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Amanksingh16/collab_FE/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Amanksingh16/collab_FE/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Amanksingh16/collab_FE/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Amanksingh16/collab_FE/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
